80c317ccd7a70e55596a5432f0d55dbb96e8d5cd
szymmar526 <szymonxmarzec@gmail.com>
2018-04-04 17:01:25 +0200

    moje
    popierdolencyitd

---
+++ Assets/Scripts/DraggableCard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Transform parentToReturnTo = null;
+	public GameObject whatToSpawn;
+
+	public void OnDrag(PointerEventData evData)
+	{
+		this.transform.position = evData.position;
+	}
+
+	public void OnBeginDrag(PointerEventData evData)
+	{
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		parentToReturnTo = this.transform.parent;
+
+		this.transform.SetParent (this.transform.parent.parent);
+
+	}
+
+	public void OnEndDrag(PointerEventData evData)
+	{
+		this.transform.SetParent (parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+	
+	
+
+
+}
